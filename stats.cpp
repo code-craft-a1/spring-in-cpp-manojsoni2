b92f8a9d3c95ba761003c368e0b50d9e95dd3130
manojsoni2 <manojsoni2@gmail.com>
2025-04-24 21:46:55 +0530

    Update stats.cpp

--- stats.cpp
+++ stats.cpp
@@ -1,5 +1,24 @@
 #include "stats.h"
+#include <algorithm>
 
-Stats Statistics::ComputeStatistics(const std::vector<___>& ) {
-    //Implement statistics here
+Statistics::Stats Statistics::ComputeStatistics(const std::vector<float>& data) {
+    Stats stats;  // Will be initialized with NaN values
+    
+    if (data.empty()) {
+        return stats;  // Returns with NaN values
+    }
+    
+    float sum = 0;
+    float currentMin = data[0];
+    float currentMax = data[0];
+    
+    for (const float& num : data) {
+        sum += num;
+        currentMin = std::min(currentMin, num);
+        currentMax = std::max(currentMax, num);
+    }
+    
+    float average = sum / static_cast<float>(data.size());
+    stats.setStats(average, currentMax, currentMin);
+    return stats;
 }
